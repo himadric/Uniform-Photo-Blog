d763b7380fb82582a71fefdbd04055c94df50b52
Himadri Chakrabarti <himadric@yahoo.com>
2023-02-13 10:12:16 -0500

    Trying to make contentful integration working.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,20 +3,33 @@ import {
   ComponentProps,
 } from "@uniformdev/canvas-react";
 
+import { ContentfulEnhancerResult } from "@uniformdev/canvas-contentful";
+
 type HeroProps = ComponentProps<{
-  title: string;
-  description?: string;
+  pageIntro: ContentfulEnhancerResult<{
+    fields: {
+      id: string;
+      pageName: string;
+      title: string;
+      body: object;
+    };
+  }>;
 }>;
 
-const Hero: React.FC<HeroProps> = ({ title, description }: HeroProps) => (
-  <div>
-    <h1 className="title">{title}</h1>
-    <div
-      className="description"
-      dangerouslySetInnerHTML={{ __html: description }}
-    />
-  </div>
-);
+const Hero: React.FC<HeroProps> = ({ pageIntro }: HeroProps) => {
+  console.log(pageIntro);
+  return (
+    <div>
+      {/* <h1 className="title">{pageIntro.fields.title["en-US"]}</h1>
+      <div
+        className="description"
+        dangerouslySetInnerHTML={{
+          __html: pageIntro.fields.body["en-US"],
+        }}
+      /> */}
+    </div>
+  );
+};
 
 registerUniformComponent({
   type: "hero",
--- lib/uniform/enhancers.ts
+++ lib/uniform/enhancers.ts
@@ -1,15 +1,32 @@
 // TODO: uncomment and bring your desired enhancer imports here
-// import { enhance, EnhancerBuilder } from "@uniformdev/canvas";
+import { compose, enhance, EnhancerBuilder } from "@uniformdev/canvas";
+import {
+  createContentfulEnhancer,
+  ContentfulClientList,
+  CANVAS_CONTENTFUL_PARAMETER_TYPES,
+} from "@uniformdev/canvas-contentful";
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: "okj8oer2ranj",
+  environment: "master",
+  accessToken: "5NzGXYkT8_-a-DreRme0JDyw8RQp7AGwchTtK3i3vHg",
+});
+const clientList = new ContentfulClientList({ client });
+const contentfulEnhancer = createContentfulEnhancer({ client: clientList });
 
 export default async function runEnhancers(composition, context) {
-  // TODO: uncomment and bring your desired enhancers here
-  // await enhance({
-  //   composition,
-  //   enhancers: new EnhancerBuilder().parameter((e) => {
-  //     if (typeof e.parameter.value === "string") {
-  //       return e.parameter.value.replace(/personalization/gi, "p13n");
-  //     }
-  //   }),
-  //   context,
-  // });
+  //TODO: uncomment and bring your desired enhancers here
+  await enhance({
+    composition,
+    enhancers: new EnhancerBuilder().parameterType(
+      CANVAS_CONTENTFUL_PARAMETER_TYPES,
+      compose(contentfulEnhancer, ({ parameter }) => {
+        const { fields } = parameter.value;
+        console.log(parameter);
+        return { fields };
+      })
+    ),
+    context: {},
+  });
 }
